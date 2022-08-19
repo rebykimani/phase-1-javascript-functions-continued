b157e6d26a9a2d44294f27ceeb16a7e2900b1bb4
beckireby15@gmail.com <beckireby15@gmail.com>
2022-08-19 16:20:09 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// code your solution here
+// // code your solution here
+// function saturdayFun(y='roller-skate!') {
+//   return `This Saturday I want to ${y}!`
+//   // console.log();
+// }
+
+// function mondayWork(x='go to the office'){
+//     // console.log (mondayWork = 'go to the office');
+// };
+// // return mondayWork(x);
+
+// function wrapAdjective(name='special') {
+//     let result = wrapAdjective(`you are ${name}` )
+//     let emphatic = result("a hard worker")
+//     }
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+const mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+function wrapAdjective(visual = "*") {
+    return function (adj = "special") {
+        return `You are ${visual}${adj}${visual}!`
+    }
+}
